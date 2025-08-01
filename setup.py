1756a43a7c15aada3cef2333aeb70d633671889e
root <root@DESKTOP-2V3IACK.localdomain>
2025-08-01 04:06:05 +0000

    Fix installation dependencies

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bjj_dojo",
+    version="0.0.1",
+    author="Dojo Planner",
+    author_email="admin@dojoplanner.com",
+    description="Complete Brazilian Jiu-Jitsu dojo management system for ERPNext",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/power19/invictus",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: ERPNext",
+        "Topic :: Office/Business",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        # Dependencies will be handled by ERPNext/Frappe
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
