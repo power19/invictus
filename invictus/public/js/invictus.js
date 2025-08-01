4cff3b379f5c460b44c482a9b46568f0087399e4
root <root@DESKTOP-2V3IACK.localdomain>
2025-08-01 04:58:51 +0000

    Add build configuration and public assets for ERPNext

---
+++ invictus/public/js/invictus.js
@@ -0,0 +1,7 @@
+// Invictus BJJ Dojo Management System
+// Main JavaScript file
+
+frappe.ready(function() {
+	// Initialize Invictus BJJ functionality
+	console.log('Invictus BJJ Dojo Management System loaded');
+});
